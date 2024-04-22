e7163e8e21e9d934023a872fdd8a1e011c6a08af
LI_MAX_ADS <3096408586@qq.com>
2024-04-22 18:28:02 +0800

    phase nearly finish

--- src/hust/cs/javacourse/search/run/TestSearchIndex.java
+++ src/hust/cs/javacourse/search/run/TestSearchIndex.java
@@ -1,9 +1,12 @@
 package hust.cs.javacourse.search.run;
 
+import hust.cs.javacourse.search.index.impl.Term;
 import hust.cs.javacourse.search.parse.AbstractTermTupleStream;
 import hust.cs.javacourse.search.query.AbstractHit;
 import hust.cs.javacourse.search.query.AbstractIndexSearcher;
 import hust.cs.javacourse.search.query.Sort;
+import hust.cs.javacourse.search.query.impl.IndexSearcher;
+import hust.cs.javacourse.search.query.impl.SimpleSorter;
 import hust.cs.javacourse.search.util.Config;
 
 import javax.swing.plaf.nimbus.AbstractRegionPainter;
@@ -17,7 +20,15 @@ public class TestSearchIndex {
      * @param args ：命令行参数
      */
     public static void main(String[] args){
+        Sort simpleSorter = new SimpleSorter();
+        String indexFile = Config.INDEX_DIR + "index.dat";
+        AbstractIndexSearcher searcher = new IndexSearcher();
+        searcher.open(indexFile);
 
+        AbstractHit[] hits = searcher.search(new Term("coronavirus"), simpleSorter);
+        for(AbstractHit hit : hits){
+            System.out.println(hit);
+        }
 
     }
 }
